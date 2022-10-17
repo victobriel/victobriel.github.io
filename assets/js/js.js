e03de30cf14eeed0fcbeb8f1819dd01c13a98ad0
victobriel <victorgbrielsilva@gmail.com>
2022-10-17 00:55:11 -0300

    Added mobile menu button

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,16 +1,114 @@
-const html = document.querySelector('html')
-const themeBtn = document.querySelector('.m-theme-switch input')
-const nameInput = document.querySelector('#name')
-const submitForm = document.querySelector('#submit-Form')
-const formMessageError = document.querySelector('#form-Error')
+const themeBtn = document.querySelector('.m-theme-switch input'),
+    submitForm = document.getElementById('form__submit'),
+    btnMobile = document.getElementById('btn-mobile'),
+    header = document.getElementById('l-header'),
+    body = document.querySelector('body'),
+    nav = document.getElementById('nav'),
+    menu = document.getElementById('menu')
 
 themeBtn.addEventListener('click', () => {
+    const html = document.querySelector('html')
     html.classList.toggle('light-mode')
 })
 
+function toggleMenu(e) {
+    if (e.type === 'touchstart') e.preventDefault()
+    header.classList.toggle('mobile-navbar')
+    body.classList.toggle('dark')
+    nav.classList.toggle('is-active')
+    const active = nav.classList.contains('is-active')
+    e.currentTarget.setAttribute('aria-expanded', active)
+    if (active)
+        e.currentTarget.setAttribute('aria-label', 'Fechar menu')
+    else
+        e.currentTarget.setAttribute('aria-label', 'Abrir menu')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
+
 submitForm.addEventListener('click', () => {
-    if (!nameInput.value.length)
-        formMessageError.innerHTML = formMessageError.innerHTML + "<p>O nome não pode estar vazio.</p>"
-    if (nameInput.value.length > 50)
-        formMessageError.innerHTML = formMessageError.innerHTML + "<p>O nome não pode conter mais de 50 caracteres.</p>"
+    alert("Sucesso")
 })
+
+menu.addEventListener('click', () => {
+    body.classList.remove('dark')
+    header.classList.remove('mobile-navbar')
+    nav.classList.remove('is-active')
+})
+
+function validateName(field) {
+    const formNameError = document.getElementById('form__name-error')
+    if (field.value.length == 0) {
+        formNameError.classList.remove('is-hidden')
+        formNameError.innerHTML = "* Nome vazio"
+        return
+    }
+    else
+    if (field.value.length > 50) {
+        formNameError.classList.remove('is-hidden')
+        formNameError.innerHTML = "* Utilize no máximo 50 caracteres para nome"
+    }
+    else
+        formNameError.classList.add('is-hidden')
+}
+
+function validateEmail(field) {
+    const formEmailError = document.getElementById('form__email-error')
+    if (field.value.length == 0){
+        formEmailError.classList.remove('is-hidden')
+        formEmailError.innerHTML = "* Email vazio"
+        return
+    }
+    let regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i
+
+    if (regex.test(field.value)) {
+        formEmailError.classList.add('is-hidden')
+        formEmailError.innerHTML = ""
+    } else {
+        formEmailError.classList.remove('is-hidden')
+        formEmailError.innerHTML = "* Email inválido"
+    }
+}
+
+function validateSubject(field) {
+    const formSubjectError = document.getElementById('form__subject-error')
+    if (field.value.length == 0) {
+        formSubjectError.classList.remove('is-hidden')
+        formSubjectError.innerHTML = "* Assunto vazio"
+        return
+    }
+    else
+    if (field.value.length > 50) {
+        formSubjectError.classList.remove('is-hidden')
+        formSubjectError.innerHTML = "* Utilize no máximo 50 caracteres para assunto"
+    } else
+        formSubjectError.classList.add('is-hidden')
+}
+
+function validateMessage(field) {
+    const formMessageError = document.getElementById('form__message-error')
+    if (field.value.length == 0) {
+        formMessageError.classList.remove('is-hidden')
+        formMessageError.innerHTML = "* Mensagem vazia"
+        return
+    }
+    else
+    if (field.value.length > 300) {
+        formMessageError.classList.remove('is-hidden')
+        formMessageError.innerHTML = "* Utilize no máximo 300 caracteres para mensagem"
+    } else
+        formMessageError.classList.add('is-hidden')
+}
+
+setInterval(() => {
+    if (window.screen.width > 768) {
+        body.classList.remove('dark')
+        header.classList.remove('mobile-navbar')
+        nav.classList.remove('is-active')
+    }
+    if (true)
+        submitForm.classList.remove('is-disabled')
+    else
+        submitForm.classList.add('is-disabled')
+}, 600)
